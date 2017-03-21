497996c64c28bb6a9f4d326ce5d698626f48e194
David Callu <callu.david@gmail.com>
2017-03-21 22:45:42 +0100

    fix(byd__package): remove useless code

--- cmake/modules/package/byd__package__component.cmake
+++ cmake/modules/package/byd__package__component.cmake
@@ -21,8 +21,6 @@ function(byd__package__add_component package component)
         )
 
     __byd__package__append_property(COMPONENTS "${component}")
-    byd__package__get_components(${package} comps)
-
     if(PARAM_VERSION)
         __byd__package__set_property(COMPONENT_${component} "${version}")
     endif()
