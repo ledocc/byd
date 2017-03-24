ee5e4a0a94b896fe9aa3f31caf8ee6fe35a09aa9
David Callu <callu.david@gmail.com>
2017-03-24 23:20:29 +0100

    package(xcb): add xcb package

---
+++ packages/xcb/version.cmake
@@ -0,0 +1,9 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+
+
+byd__package__add_version(
+    ${package} 1.11
+    URL "https://xcb.freedesktop.org/dist/libxcb-1.11.tar.bz2"
+    URL_HASH SHA1=8343b417d7eeb2a2c6b6c4a87a03a4fd0fc65c46
+    )
