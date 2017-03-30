8ea7d5c2c7596f1206e2623cd1377f2afacb55d4
David Callu <callu.david@gmail.com>
2017-03-30 08:13:39 +0200

    feat(freetype): update version from 2.6.3 to 2.7.1

--- packages/freetype/version.cmake
+++ packages/freetype/version.cmake
@@ -3,7 +3,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 
 byd__package__add_version(
-    ${package} 2.6.3
-    URL "https://sourceforge.net/projects/freetype/files/freetype2/2.6.3/freetype-2.6.3.tar.bz2"
-    URL_HASH SHA1=6c98bd5d0be313207c37ca23d25faf983486aee5
+    ${package} 2.7.1
+    URL "https://sourceforge.net/projects/freetype/files/freetype2/2.7.1/freetype-2.7.1.tar.bz2"
+    URL_HASH SHA1=4d08a9a6567c6332d58e9a5f9a7e9e3fbce66789
     )
