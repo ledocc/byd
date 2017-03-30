0b02b426bbc0e38d2c86e3e6fbaf581ef492a8a3
David Callu <callu.david@gmail.com>
2017-03-30 08:12:13 +0200

    feat(windows): add support of windows build for GoogleBreakpad Poco icu

--- packages/Poco/desc.cmake
+++ packages/Poco/desc.cmake
@@ -1,3 +1,6 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+
 
 byd__package__add_dependency_constraint(Poco OpenSSL VERSION_GREATER_EQUAL 1.0.2)
 byd__package__add_dependency_constraint(Poco zip VERSION_GREATER_EQUAL 2.6.7)
