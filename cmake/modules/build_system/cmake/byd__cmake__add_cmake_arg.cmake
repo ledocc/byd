a0d0d074696ebb366ba0ed8989e19844754c1fd7
David Callu <callu.david@gmail.com>
2017-03-30 08:03:08 +0200

    refacto(byd__cmake): change byd__cmake__add_cmake_arg to byd__cmake__configure__add_arg

--- cmake/modules/build_system/cmake/byd__cmake__add_cmake_arg.cmake
+++ cmake/modules/build_system/cmake/byd__cmake__add_cmake_arg.cmake
@@ -5,7 +5,7 @@
 ##--------------------------------------------------------------------------------------------------------------------##
 ##--------------------------------------------------------------------------------------------------------------------##
 
-function(byd__cmake__add_cmake_arg package variable value)
+function(byd__cmake__configure__add_arg package variable value)
 
     byd__func__add_to_property(BYD__EP__CONFIGURE__CMAKE_ARGS__${package} "-D${variable}=${value}")
 
--- packages/Qt5/5.7.1/options/_unix.cmake
+++ packages/Qt5/5.7.1/options/_unix.cmake
@@ -1,6 +1,8 @@
 
-byd__Qt5__configure__add_args(${package} "-I${CMAKE_INSTALL_PREFIX}/include")
-byd__Qt5__configure__add_args(${package} "-L${CMAKE_INSTALL_PREFIX}/lib")
+if(CMAKE_INSTALL_PREFIX)
+    byd__Qt5__configure__add_args(${package} "-I" "${CMAKE_INSTALL_PREFIX}/include")
+    byd__Qt5__configure__add_args(${package} "-L" "${CMAKE_INSTALL_PREFIX}/lib")
+endif()
 byd__Qt5__configure__add_args(${package} -rpath)
 byd__Qt5__configure__add_args(${package} -continue)
 
