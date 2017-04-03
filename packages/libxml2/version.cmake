d9f1c94304961c4e160ed5628493e8623e9f4858
David Callu <callu.david@gmail.com>
2017-04-03 11:57:45 +0200

    feat(package libxml2): add libxml2

---
+++ packages/libxml2/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 2.9.4
+    URL "ftp://xmlsoft.org/libxml2/libxml2-2.9.4.tar.gz"
+    URL_HASH SHA1=958ae70baf186263a4bd801a81dd5d682aedd1db
+)
---
+++ packages/libxslt/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 1.1.29
+    URL "ftp://xmlsoft.org/libxml2/libxslt-1.1.29.tar.gz"
+    URL_HASH SHA1=edcaeabb3555ae44853bdc406ee9521fb65c620d
+)
