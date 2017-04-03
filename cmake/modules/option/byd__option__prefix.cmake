bf3f7767a5e668f5efd069024631e8c5160bcbc4
David Callu <callu.david@gmail.com>
2017-04-03 12:08:15 +0200

    feat(byd__option__prefix): get value from environment variable

--- cmake/modules/option/byd__option__prefix.cmake
+++ cmake/modules/option/byd__option__prefix.cmake
@@ -9,8 +9,16 @@ include("${BYD_ROOT}/cmake/modules/byd__prefix.cmake")
 
 function(byd__option__prefix)
 
-    set(BYD__OPTION__PREFIX "" CACHE STRING "Specifies tge path where packages are build")
-    byd__set_prefix(${BYD__OPTION__PREFIX})
+    set(default_prefix "")
+
+    set(prefix_from_env_var "$ENV{BYD__OPTION__PREFIX}")
+    if(prefix_from_env_var)
+        set(default_prefix "${prefix_from_env_var}")
+    endif()
+    
+    set(BYD__OPTION__PREFIX "${default_prefix}" CACHE STRING "Specifies the path where packages are build. Usefull on Windows where path have limited length.")
+    file(TO_CMAKE_PATH "${BYD__OPTION__PREFIX}" BYD__OPTION__PREFIX_CMAKE_PATH)
+    byd__set_prefix("${BYD__OPTION__PREFIX_CMAKE_PATH}")
 
     variable_watch(BYD__OPTION__PREFIX byd__private__update_option_prefix)
 
@@ -22,7 +30,8 @@ function(byd__private__update_option_prefix variable access value current_list_f
     cmut__variable_watch__log_arguments("${variable}" "${access}" "${value}" "${current_list_file}" "${stack}")
 
     if(access STREQUAL "MODIFIED_ACCESS")
-        byd__set_prefix("${value}")
+        file(TO_CMAKE_PATH "${value}" value_CMAKE_PATH)
+        byd__set_prefix("${value_CMAKE_PATH}")
     endif()
 
 endfunction()
