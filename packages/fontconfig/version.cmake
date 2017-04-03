92cc6a60630a3a823b014b771a9d81b736da2b0a
David Callu <callu.david@gmail.com>
2017-04-03 11:56:14 +0200

    feat(package fontconfig): add fontconfig

---
+++ packages/fontconfig/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 2.12.1
+    URL "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.1.tar.bz2"
+    URL_HASH SHA1=30d832b754fb10a3b70ebac750a38a0275438ad8
+)
