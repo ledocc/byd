7f561550b5a6c8d9a198378fbae64f51fc881447
David Callu <callu.david@gmail.com>
2017-04-03 11:56:42 +0200

    feat(package expat): add expat

---
+++ packages/expat/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 2.2.0
+    URL "https://downloads.sourceforge.net/project/expat/expat/2.2.0/expat-2.2.0.tar.bz2"
+    URL_HASH SHA1=8453bc52324be4c796fd38742ec48470eef358b3
+)
