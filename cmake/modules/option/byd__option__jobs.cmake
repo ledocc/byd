e4823342c5757cf4989c73c9341ce0799df2f2a6
David Callu <callu.david@gmail.com>
2017-04-03 12:07:04 +0200

    feat(byd__option__jobs): get value from enviropnment variable, then from system

--- cmake/modules/option/byd__option__jobs.cmake
+++ cmake/modules/option/byd__option__jobs.cmake
@@ -9,8 +9,16 @@ include("${BYD_ROOT}/cmake/modules/private/byd__private__num_core_available.cmak
 
 function(byd__option__jobs)
 
-    cmut__system__get_num_core_available(num_core)
-    set(BYD__OPTION__JOBS ${num_core} CACHE STRING "Specifies the number of jobs to run simultaneously. default = num of logical core on the host")
+    set(default_jobs "")
+
+    set(jobs_from_env_var "$ENV{BYD__OPTION__JOBS}")
+    if(jobs_from_env_var)
+        set(default_jobs "${jobs_from_env_var}")
+    else()
+        cmut__system__get_num_core_available(default_jobs)
+    endif()
+    
+    set(BYD__OPTION__JOBS ${default_jobs} CACHE STRING "Specifies the number of jobs to run simultaneously. default = num of logical core on the host")
     byd__private__set_num_core_available(${BYD__OPTION__JOBS})
 
     variable_watch(BYD__OPTION__JOBS byd__private__update_option_jobs)
