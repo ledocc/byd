cf6d0eac47e7f4450279b10253de34e67c4dbb1b
David Callu <callu.david@gmail.com>
2017-04-03 11:55:31 +0200

    feat(package xz-util): add xz-util

---
+++ packages/xz-util/dependency.cmake
@@ -0,0 +1,4 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__set_dependency(${package}
+    )
---
+++ packages/xz-util/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 5.2.3
+    URL "http://tukaani.org/xz/xz-5.2.3.tar.xz"
+    URL_HASH SHA1=a2975d12e0905daec48ec87c0098602e0669d195
+)
