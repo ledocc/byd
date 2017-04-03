53307544c2d1ff0e375b0b6cd8abdb6583073ae1
David Callu <callu.david@gmail.com>
2017-04-03 11:57:18 +0200

    feat(package libwebp): add libwebp

---
+++ packages/libwebp/version.cmake
@@ -0,0 +1,8 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 0.6.0
+    URL "https://github.com/webmproject/libwebp/archive/v0.6.0.tar.gz"
+    URL_HASH SHA1=7294e5571657b8acdc3048d0a2d7a4cf746f5c80
+#    URL_HASH SHA1=55e7b93f8dbf52bb68d85ccbc6df5f4e3b6220ea
+)
