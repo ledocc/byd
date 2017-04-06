5e9d6b560505e4a2d4f87e3cec9379f2ad08bc8c
David Callu <callu.david@gmail.com>
2017-04-06 15:01:24 +0200

    fix(LibVNSServer): change version to build on Android

--- packages/LibVNCServer/version.cmake
+++ packages/LibVNCServer/version.cmake
@@ -7,3 +7,9 @@ byd__package__add_version(
     URL "https://github.com/ledocc/LibVNCServer/archive/0.9.11-unowhy.tar.gz"
     URL_HASH SHA1=c714bdb4369cc09c79783af95d60dbda462f83a5
     )
+
+byd__package__add_version(
+    ${package} 0.9.11-unowhy-1
+    URL "https://github.com/ledocc/libvncserver/archive/0.9.11-unowhy-1.tar.gz"
+    URL_HASH SHA1=af9a54d6afeca71022b8ee97b50f28b9a69b6be4
+    )
