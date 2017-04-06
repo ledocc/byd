d72b1256d2c7e7b589448b4cd67059cf64ce9e76
David Callu <callu.david@gmail.com>
2017-04-06 15:10:03 +0200

    feat(QuaZip): add QuaZip package

---
+++ packages/QuaZip/version.cmake
@@ -0,0 +1,9 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+
+
+byd__package__add_version(
+    ${package} 0.7.2-unowhy
+    URL "https://github.com/ledocc/QuaZip/archive/v0.7.2-unowhy.tar.gz"
+    URL_HASH SHA1=af166fcfd132b245d6cf5050bda30dcec1e73794
+    )
