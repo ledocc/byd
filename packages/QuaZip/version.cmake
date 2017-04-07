eb418b3198f4d97b812ec934cf3eefddc803b301
David Callu <callu.david@gmail.com>
2017-04-07 10:50:26 +0200

    fix(QuaZip): change wrong sha1 hash to version 0.7.2-unowhy

--- packages/QuaZip/version.cmake
+++ packages/QuaZip/version.cmake
@@ -5,5 +5,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 byd__package__add_version(
     ${package} 0.7.2-unowhy
     URL "https://github.com/ledocc/QuaZip/archive/v0.7.2-unowhy.tar.gz"
-    URL_HASH SHA1=af166fcfd132b245d6cf5050bda30dcec1e73794
+    URL_HASH SHA1=429794c276fb4458c2123356969609932875c396
     )
