63156dea4283496c1439b55537690a503c666e43
David Callu <callu.david@gmail.com>
2017-04-19 16:27:11 +0200

    feat(package): add libzippp

---
+++ packages/libzippp/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 2.1-1.2.0-unowhy
+    URL "https://github.com/ledocc/libzippp/archive/libzippp-v2.1-1.2.0-unowhy.tar.gz"
+    URL_HASH SHA1=db572d0d37c3b6495d84f342bf19ec6c81b088b7
+)
