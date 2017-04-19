1a820985fdf95fade82f359fc6bd40d87e34204d
David Callu <callu.david@gmail.com>
2017-04-19 16:25:52 +0200

    feat(package): add libzip

---
+++ packages/libzip/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 1.2.0-unowhy
+    URL "https://github.com/ledocc/libzip/archive/rel-1.2.0-unowhy.tar.gz"
+    URL_HASH SHA1=fa71f3b1eb62c714e01465e04d9e916f8c410227
+)
