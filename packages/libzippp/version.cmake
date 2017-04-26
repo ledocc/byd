50cd1b5fd8b22c921f23c38b8a7977d80731957e
David Callu <callu.david@gmail.com>
2017-04-26 09:30:45 +0200

    fix(libzippp): update hash

--- packages/libzippp/version.cmake
+++ packages/libzippp/version.cmake
@@ -3,5 +3,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 byd__package__add_version(
     ${package} 2.1-1.2.0-unowhy
     URL "https://github.com/ledocc/libzippp/archive/libzippp-v2.1-1.2.0-unowhy.tar.gz"
-    URL_HASH SHA1=db572d0d37c3b6495d84f342bf19ec6c81b088b7
+    URL_HASH SHA1=e0bc0f43556516b4e5d56e84353812ab8fd69056
 )
