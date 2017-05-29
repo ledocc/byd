92a5651944d26736a8379ede08e4fa8f46630aed
David Callu <callu.david@gmail.com>
2017-05-29 09:25:52 +0200

    feat(openssl): use github repository, and add version 1.0.2l

--- packages/OpenSSL/version.cmake
+++ packages/OpenSSL/version.cmake
@@ -1,6 +1,11 @@
 include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 byd__package__add_version(${package} 1.0.2k
-    URL "https://gitlab.unowhy.com/unowhy-sqool/openssl/repository/archive.tar.bz2?ref=OpenSSL_1_0_2k"
-    URL_HASH MD5=9ab0c549d179ec7d578b7de18823cc0d
+    URL "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2k.tar.gz"
+    URL_HASH MD5=462944eff7b045d950deaaa86798190cbdc5278a
+    )
+
+byd__package__add_version(${package} 1.0.2l
+    URL "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2l.tar.gz"
+    URL_HASH MD5=5bea0957b371627e8ebbee5bef221519e94d547c
     )
