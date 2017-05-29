8b6d2415fb67a6d87ca1eff38c9d2d3df8bba8ef
David Callu <callu.david@gmail.com>
2017-05-29 14:13:19 +0200

    fix(OpenSSL): fix openssl URL_HASH

--- packages/OpenSSL/version.cmake
+++ packages/OpenSSL/version.cmake
@@ -2,10 +2,10 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 byd__package__add_version(${package} 1.0.2k
     URL "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2k.tar.gz"
-    URL_HASH MD5=462944eff7b045d950deaaa86798190cbdc5278a
+    URL_HASH SHA1=462944eff7b045d950deaaa86798190cbdc5278a
     )
 
 byd__package__add_version(${package} 1.0.2l
     URL "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2l.tar.gz"
-    URL_HASH MD5=5bea0957b371627e8ebbee5bef221519e94d547c
+    URL_HASH SHA1=5bea0957b371627e8ebbee5bef221519e94d547c
     )
