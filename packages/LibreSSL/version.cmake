874fc88323836f43ffce83a2a1e863d38c441784
David Callu <callu.david@gmail.com>
2017-06-18 01:47:16 +0200

    feat(package): add LibreSSL

---
+++ packages/LibreSSL/version.cmake
@@ -0,0 +1,7 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+byd__package__add_version(
+    ${package} 2.5.4
+    URL "https://github.com/libressl-portable/portable/archive/v2.5.4.tar.gz"
+    URL_HASH SHA1=4925000077c641d52b7388587f52c18f5b1d812b
+)
