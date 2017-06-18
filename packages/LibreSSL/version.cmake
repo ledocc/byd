eb42cbb825e7dd60b2015a941494a8d7be4a6f0b
David Callu <callu.david@gmail.com>
2017-06-18 17:19:36 +0200

    fix(LibreSSL): use package from openbsd.org

--- packages/LibreSSL/version.cmake
+++ packages/LibreSSL/version.cmake
@@ -2,6 +2,6 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 byd__package__add_version(
     ${package} 2.5.4
-    URL "https://github.com/libressl-portable/portable/archive/v2.5.4.tar.gz"
-    URL_HASH SHA1=4925000077c641d52b7388587f52c18f5b1d812b
+    URL "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.5.4.tar.gz"
+    URL_HASH SHA1=1fa2eb0b1c8285a4f51132ac78cd8c95f302b768
 )
