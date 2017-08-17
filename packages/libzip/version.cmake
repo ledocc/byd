94a2c46eac94a736f51a6258f78411a845711380
David Callu <callu.david@gmail.com>
2017-08-17 23:39:26 +0200

    feat(libzip): add version 1.2.0-unowhy-p2 with option to build static or shared library

--- packages/libzip/version.cmake
+++ packages/libzip/version.cmake
@@ -11,3 +11,9 @@ byd__package__add_version(
     URL "https://github.com/ledocc/libzip/archive/rel-1.2.0-unowhy-p1.tar.gz"
     URL_HASH SHA1=94d29ddd4540de7a85a20a1b068a98bb88ae0a2e
 )
+
+byd__package__add_version(
+    ${package} 1.2.0-unowhy-p2
+    URL "https://github.com/ledocc/libzip/archive/rel-1.2.0-unowhy-p2.tar.gz"
+    URL_HASH SHA1=49e330f0808f015f412a2b06dc5237f8d1b6bb94
+)
