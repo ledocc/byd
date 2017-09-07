ac9080d851f587e1663fbfcce7d3ebf301e1f3aa
David Callu <callu.david@gmail.com>
2017-09-07 14:34:56 +0200

    feat(libwebp): add component

---
+++ packages/libwebp/component.cmake
@@ -0,0 +1,8 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+
+
+byd__package__add_component(${package} libwepmux)
+byd__package__add_component(${package} libwebpdemux)
+byd__package__add_component(${package} libwebpdecoder)
+byd__package__add_component(${package} libwebpextras)
