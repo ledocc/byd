ad92381e15d2b30f0a2a77d47a0bc9c296c1e661
David Callu <callu.david@gmail.com>
2017-09-08 11:29:08 +0200

    feat(Boost): add version 1.65.1

--- packages/Boost/version.cmake
+++ packages/Boost/version.cmake
@@ -11,3 +11,9 @@ byd__package__add_version(
     URL "https://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.bz2"
     URL_HASH SHA1=51421ef259a4530edea0fbfc448460fcc5c64edb
     )
+
+byd__package__add_version(
+    ${package} 1.65.1
+    URL "https://sourceforge.net/projects/boost/files/boost/1.65.1/boost_1_65_1.tar.bz2"
+    URL_HASH SHA1=4a5b0c3c1b1b9a4d6cb6a6cc395e903e76f76720
+    )
