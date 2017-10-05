1faaecb43df1ecf4ec2fd3de1185d12156b5dfb7
David Callu <callu.david@gmail.com>
2017-10-05 07:57:31 +0200

    feat(LibreSSL): add version 2.5.5

--- packages/LibreSSL/version.cmake
+++ packages/LibreSSL/version.cmake
@@ -5,3 +5,9 @@ byd__package__add_version(
     URL "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.5.4.tar.gz"
     URL_HASH SHA1=1fa2eb0b1c8285a4f51132ac78cd8c95f302b768
 )
+
+byd__package__add_version(
+    ${package} 2.5.5
+    URL "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.5.5.tar.gz"
+    URL_HASH SHA1=36511c98fe450bbb50da8c8a3d4eba2cc7d0f83c
+)
