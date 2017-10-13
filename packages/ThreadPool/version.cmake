dbcf4e8e5cfadf5308a0c712d237295a24ef3ba6
David Callu <callu.david@gmail.com>
2017-10-13 16:01:20 +0200

    feat(package): add ThreadPool

---
+++ packages/ThreadPool/version.cmake
@@ -0,0 +1,9 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+
+
+byd__package__add_version(
+    ${package} 0.1
+    URL "https://github.com/ledocc/ThreadPool/archive/0.1.tar.gz"
+    URL_HASH SHA1=1c537fbba3556c3fe29770a917e1143eb2d00928
+    )
