67dbcd59ef200b6aa96e689b9a1883e5a3769ecc
David Callu <callu.david@gmail.com>
2017-10-19 14:50:54 +0200

    fix(libwebp): change component from libwepmux to libwebpmux

--- packages/libwebp/component.cmake
+++ packages/libwebp/component.cmake
@@ -2,7 +2,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 
 
-byd__package__add_component(${package} libwepmux)
+byd__package__add_component(${package} libwebpmux)
 byd__package__add_component(${package} libwebpdemux)
 byd__package__add_component(${package} libwebpdecoder)
 byd__package__add_component(${package} libwebpextras)
