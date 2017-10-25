2a3c7ee658f1c8eef7e34d20cd35e8fd9406bd97
David Callu <callu.david@gmail.com>
2017-10-25 21:33:34 +0200

    feat(cmake): print byd path/version

--- byd.cmake
+++ byd.cmake
@@ -17,5 +17,7 @@ if(CMUT_VERSION VERSION_LESS 0.5.0)
     message(FATAL_ERROR "byd require cmut version 0.5.0 or greater.")
 endif()
 
-
+set(BYD_ROOT "${CMAKE_CURRENT_LIST_DIR}")
 include("${BYD_ROOT}/cmake/modules.cmake")
+cmut_info("[byd] - byd location  : \"${BYD_ROOT}\".")
+cmut_info("[byd] - byd version   : \"${BYD_VERSION}\".")
--- cmake/modules.cmake
+++ cmake/modules.cmake
@@ -2,6 +2,7 @@ include("${BYD_ROOT}/cmake/modules/byd__add_package.cmake")
 include("${BYD_ROOT}/cmake/modules/byd__get_build_id.cmake")
 include("${BYD_ROOT}/cmake/modules/byd__initialize.cmake")
 include("${BYD_ROOT}/cmake/modules/byd__run.cmake")
+include("${BYD_ROOT}/cmake/modules/byd_version.cmake")
 
 include("${BYD_ROOT}/cmake/modules/action.cmake")
 include("${BYD_ROOT}/cmake/modules/archive.cmake")
