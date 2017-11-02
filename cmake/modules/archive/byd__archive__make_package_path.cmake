5f0f7b995c4fe57e8af442e6a10a315a08a87d56
David Callu <callu.david@gmail.com>
2017-11-02 16:04:28 +0100

    refactor(archive): add variables to cmake_args_build_id

--- cmake/modules/archive/byd__archive__make_package_path.cmake
+++ cmake/modules/archive/byd__archive__make_package_path.cmake
@@ -30,17 +30,50 @@ endfunction()
 
 ##--------------------------------------------------------------------------------------------------------------------##
 
+function(__byd__archive__add_cmake_args_build_id args variable )
+
+    if(NOT DEFINED ${variable})
+        return()
+    endif()
+
+    list(APPEND ${args} ${variable})
+    set(${args} "${${args}}" PARENT_SCOPE)
+
+endfunction()
+
+##--------------------------------------------------------------------------------------------------------------------##
+
 function(byd__archive__get_cmake_args_build_id package result)
 
     set(CMAKE_ARGS
-        CMAKE_C_COMPILER
-        CMAKE_CXX_COMPILER
         BUILD_SHARED_LIBS
         CMAKE_BUILD_TYPE
+        CMAKE_C_COMPILER
+        CMAKE_C_COMPILER_AR
+        CMAKE_C_COMPILER_RANLIB
+        CMAKE_C_FLAGS
+        CMAKE_CXX_COMPILER
+        CMAKE_CXX_COMPILER_AR
+        CMAKE_CXX_COMPILER_RANLIB
+        CMAKE_CXX_FLAGS
+        CMAKE_EXE_LINKER_FLAGS
+        CMAKE_LINKER
+        CMAKE_MODULE_LINKER_FLAGS
         CMAKE_OSX_ARCHITECTURES
         CMAKE_OSX_DEPLOYMENT_TARGET
         CMAKE_OSX_SYSROOT
+        CMAKE_SHARED_LINKER_FLAGS
+        CMAKE_STATIC_LINKER_FLAGS
         )
+
+    if (DEFINED CMAKE_BUILD_TYPE)
+        string(TOUPPER ${CMAKE_BUILD_TYPE} buildType)
+        __byd__archive__add_cmake_args_build_id(CMAKE_ARGS CMAKE_C_FLAGS_${buildType})
+        __byd__archive__add_cmake_args_build_id(CMAKE_ARGS CMAKE_CXX_FLAGS_${buildType})
+        __byd__archive__add_cmake_args_build_id(CMAKE_ARGS CMAKE_EXE_LINKER_FLAGS_${buildType})
+        __byd__archive__add_cmake_args_build_id(CMAKE_ARGS CMAKE_MODULE_LINKER_FLAGS_${buildType})
+        __byd__archive__add_cmake_args_build_id(CMAKE_ARGS CMAKE_SHARED_LINKER_FLAGS_${buildType})
+    endif()
 
     foreach(arg IN LISTS CMAKE_ARGS)
         list(APPEND cmake_args "${arg}=${${arg}}")
