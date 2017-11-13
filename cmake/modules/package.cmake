bed4421810baae6239e2d4a0e48a6140da122c80
David Callu <callu.david@gmail.com>
2017-11-13 23:05:14 +0100

    feat(package): add package definition helpers

--- cmake/modules/package.cmake
+++ cmake/modules/package.cmake
@@ -1,4 +1,5 @@
 include("${BYD_ROOT}/cmake/modules/package/byd__package__component.cmake")
+include("${BYD_ROOT}/cmake/modules/package/byd__package__definition_helpers.cmake")
 include("${BYD_ROOT}/cmake/modules/package/byd__package__dependency.cmake")
 include("${BYD_ROOT}/cmake/modules/package/byd__package__directory.cmake")
 include("${BYD_ROOT}/cmake/modules/package/byd__package__info.cmake")
---
+++ cmake/modules/package/byd__package__definition_helpers.cmake
@@ -0,0 +1,34 @@
+
+
+function(byd__package__check_component package)
+
+    cmut_debug("package = ${package}")
+    byd__package__split_package_component_name(${package} package_name component_name)
+
+    cmut_debug("package_name = ${package_name}, component_name = ${component_name}")
+
+    byd__package__assert_not_empty_component(${package_name} ${component_name})
+    byd__package__assert_component_exists(${package_name} ${component_name})
+
+endfunction()
+
+function(byd__package__assert_not_empty_component package_name component_name)
+
+    byd__private__is_empty(component_name is_empty_component_name)
+
+    if(is_empty_component_name)
+        cmut_fatal("[byd][package] - [${package_name}] : no component to build.")
+    endif()
+
+endfunction()
+
+
+
+function(byd__package__assert_component_exists package_name component_name)
+
+    byd__package__get_components(${package_name} components)
+    if(NOT component_name IN_LIST components)
+        cmut_fatal("[byd][package] - [${package_name}] : component \"${component_name}\" requested but not provide by package definition.")
+    endif()
+
+endfunction()
