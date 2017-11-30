272bf36151dc9fe6b77716251ea21a0f4d3a1e63
David Callu <callu.david@gmail.com>
2017-11-30 07:15:34 +0100

    fix(option): use USERPROFILE env var on windows for default_local_repo

--- cmake/modules/option/byd__option__local_repo.cmake
+++ cmake/modules/option/byd__option__local_repo.cmake
@@ -8,8 +8,14 @@ include("${BYD_ROOT}/cmake/modules/option/private/make_absolute_cmake_path.cmake
 
 function(byd__option__local_repo)
 
+    if(CMAKE_HOST_WIN32)
+        set(local_repo "$ENV{USERPROFILE}/.byd")
+    else()
+        set(local_repo "$ENV{HOME}/.byd")
+    endif()
 
-    byd__option__private__get_default("BYD__OPTION__LOCAL_REPO" "$ENV{HOME}/.byd" default_local_repo)
+
+    byd__option__private__get_default("BYD__OPTION__LOCAL_REPO" "${local_repo}" default_local_repo)
 
     byd__option__private__make_absolute_cmake_path("${default_local_repo}" default_local_repo__absolute_cmake_path)
     set(BYD__OPTION__LOCAL_REPO
