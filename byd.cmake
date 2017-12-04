b009bfd4de6963c5ba92555551375f27e9cbf286
David Callu <callu.david@gmail.com>
2017-12-04 16:08:44 +0100

    refactor(byd): no longer print cmut version in byd.cmake

--- byd.cmake
+++ byd.cmake
@@ -10,14 +10,17 @@ if(NOT DEFINED CMUT_ROOT)
 endif()
 
 include(${CMUT_ROOT}/cmut.cmake)
-cmut_info("[byd] - cmut location : \"${CMUT_ROOT}\".")
-cmut_info("[byd] - cmut version  : \"${CMUT_VERSION}\".")
 
 if(CMUT_VERSION VERSION_LESS 0.5.0)
     message(FATAL_ERROR "byd require cmut version 0.5.0 or greater.")
 endif()
 
+
+
 set(BYD_ROOT "${CMAKE_CURRENT_LIST_DIR}")
 include("${BYD_ROOT}/cmake/modules.cmake")
-cmut_info("[byd] - byd location  : \"${BYD_ROOT}\".")
-cmut_info("[byd] - byd version   : \"${BYD_VERSION}\".")
+
+if(NOT BYD__MAIN_INCLUDE_QUIET)
+    cmut_info("[byd] - location  : \"${BYD_ROOT}\".")
+    cmut_info("[byd] - version   : \"${BYD_VERSION}\".")
+endif()
