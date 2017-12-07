c3d6aa63c076bbb1fcfbbf27d3c42b8e69277aca
David Callu <callu.david@gmail.com>
2017-12-07 19:19:08 +0100

    fix(option): make BYD__OPTION__UPLOAD_ARCHIVE a boolean option

--- cmake/modules/option/byd__option__upload_archive.cmake
+++ cmake/modules/option/byd__option__upload_archive.cmake
@@ -8,15 +8,11 @@ function(byd__option__upload_archive)
 
     byd__option__private__get_default("BYD__OPTION__UPLOAD_ARCHIVE" "OFF" default_upload_archive)
 
-    set(BYD__OPTION__UPLOAD_ARCHIVE
-        "${default_upload_archive}"
-        CACHE
-        PATH
-        "Enable to upload archive on remote repo defined by BYD__OPTION__REMOTE_REPO."
-        FORCE
+    option(
+        BYD__OPTION__UPLOAD_ARCHIVE 
+        "Enable to upload archive on remote repo defined by BYD__OPTION__REMOTE_REPO." 
+        ${default_upload_archive}
         )
-
-    option(BYD__OPTION__UPLOAD_ARCHIVE "Enable to upload archive." ${default_upload_archive})
 
 endfunction()
 
